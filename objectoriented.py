0f5cb26e57d0c6f2dc5953915c4df6f02da7bb6a
xgerg <gregghuntt@yahoo.com>
2024-12-02 13:07:51 -0500

    Update objectoriented.py

--- objectoriented.py
+++ objectoriented.py
@@ -1,7 +1,14 @@
+#This will be an object oriented version
+#of the virtual3d game
 import cv2
 import numpy
 
 print('Starting OO virtual3d')
+
+
+class Tunnel:
+	pass
+
 
 class FaceFinder:
 	"""Use haar cascade filter to detect largest face from a frame."""
@@ -22,10 +29,8 @@ class FaceFinder:
 		for (x,y,w,h) in faces:
 			if w > bw:
 				bx,by,bw,bh = x,y,w,h
-		cv2.rectangle(img, (bx, by), (bx+bw, by+bh), (0, 255, 255), 5)
+		cv2.rectangle(frame, (bx, by), (bx+bw, by+bh), (0, 255, 255), 5)
 		return (bx+bw/2),(by+bh/2)
-
-
 
 
 #-------------------------------------
@@ -33,4 +38,32 @@ class FaceFinder:
 #
 
 ff = FaceFinder()
+cap = cv2.VideoCapture(cv2.CAP_ANY)
+if not cap.isOpened():
+	print('couldnt open cam')
+	exit()
+
+
+
+
+while True:
+  retval, frame = cap.read()
+  if retval == False:
+    print('camera error!')
+
+  ff.find_face(frame)
+  cv2.imshow('q to quit', frame)
+
+  if cv2.waitKey(30) == ord('q'):
+    break
+
+
+
+pause = input('press enter to end')
+
+cap.release()
+
+cv2.destroyAllWindows()
+
+
 print('virtual3d complete')
